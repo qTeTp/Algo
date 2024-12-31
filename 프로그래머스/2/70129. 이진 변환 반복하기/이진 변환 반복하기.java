ebe067af686bd242f4906650931b932e4d7e9e4a
qTeTp <68058317+qTeTp@users.noreply.github.com>
2024-12-31 13:07:48 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 714.12 ms, Memory: 380 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -1,21 +1,25 @@
-class Solution { // 이진 변환 반복하기
-	public int[] solution(String s) {
-		int[] answer = new int[2];
+// 프로그래머스 이진 변환 반복하기
+// 0 제거 후 남은 1의 길이 -> 이진 변환 반복
+import java.util.*;
 
-		while (s.length() > 1) {
+class Solution {
+    public int[] solution(String s) {
+        // 이진 변환 턴 회수, 제거한 0의 개수
+        int turn = 0, del = 0;
+        // s의 길이가 1보다 클 때만 반복
+        while (s.length() > 1) {
+            String tmp = "";
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '0') {
+                    del++;
+                    continue;
+                }
+                tmp += s.charAt(i);
+            }
+            s = Integer.toBinaryString(tmp.length());
+            turn++;
+        }
 
-			int cnt = 0; // 1이진법 카운트
-			for (int i = 0; i < s.length(); i++) {
-
-				if (s.charAt(i) == '0') { // 0이면 제거
-					answer[1]++; // '0'의 개수를 세는 변수
-				} else { // 1일때
-					cnt++;
-				}
-			}
-			s = Integer.toString(cnt, 2);
-			answer[0]++;
-		}
-		return answer;
-	}
+        return new int[] { turn, del };
+    }
 }
