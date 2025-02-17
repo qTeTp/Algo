c7b074e52223426d2f59d2fc26745a16b75cd956
qTeTp <68058317+qTeTp@users.noreply.github.com>
2025-02-17 20:07:16 +0900

    [level 2] Title: 타겟 넘버, Time: 203.03 ms, Memory: 139 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,14 +1,16 @@
 // 프로그래머스 깊이/너피 우선 탐색(DFS/BFS)
 // 양의 정수의 배열 numbers, 양의 정수 타겟 넘버 target이 배개변수로 주어짐
 // numbers의 수들을 적절히 더하고 빼서 만들수 있는 target의 방법의 수를 반환
+import java.util.*;
+
 class Solution {
     static int answer = 0;
 
     public int solution(int[] numbers, int target) {
         // dfs
-        dfs(numbers, target, 0, 0);
+        // dfs(numbers, target, 0, 0);
         // bfs
-        // bfs(numbers);
+        bfs(numbers, target);
         return answer;
     }
 
@@ -28,7 +30,26 @@ class Solution {
     }
 
     // bfs - 큐 사용
-    public void bfs(int[] numbers) {
+    public void bfs(int[] numbers, int target) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(0);
+        // 깊이까지
+        for (int i = 0; i < numbers.length; i++) {
+            // 너비까지
+            int size = queue.size();
+            for (int j = 0; j < size; j++) {
+                int sum = queue.poll();
+                // 무다무다 계속 넣어줌
+                queue.add(sum + numbers[i]);
+                queue.add(sum - numbers[i]);
+            }
+        }
 
+        // 들어가있는거 모두 확인
+        while (!queue.isEmpty()) {
+            if (queue.poll() == target) {
+                answer++;
+            }
+        }
     }
 }
