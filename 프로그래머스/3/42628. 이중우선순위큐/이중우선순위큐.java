7978455b1279122fc3499c2b2265d6b98c31ed47
qTeTp <68058317+qTeTp@users.noreply.github.com>
2025-06-17 18:44:13 +0900

    [level 3] Title: 이중우선순위큐, Time: 66.70 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,12 +1,12 @@
 import java.util.*;
 
 // 이중우선순위큐(힙)
+// 우선순위큐 사용
 class Solution {
     public int[] solution(String[] operations) {
-        // 큐 두개 사용 정방향, 역방향, q의 크기
+        // 큐 두개 사용 정방향, 역방향
         PriorityQueue<Integer> fqueue = new PriorityQueue<>();
         PriorityQueue<Integer> rqueue = new PriorityQueue<>(Collections.reverseOrder());
-        int qSize = 0;
 
         for (String str : operations) {
             String[] tmp = str.split(" ");
@@ -14,23 +14,20 @@ class Solution {
             if (tmp[0].equals("I")) {
                 fqueue.offer(Integer.parseInt(tmp[1]));
                 rqueue.offer(Integer.parseInt(tmp[1]));
-                qSize++;
-            } else if (tmp[0].equals("D") && qSize > 0) {
+
+            } else if (tmp[0].equals("D") && fqueue.size() > 0) {
                 // -1 : 최솟값 삭제, 1 : 최댓값 삭제
                 if (Integer.parseInt(tmp[1]) == -1) {
-                    int min = fqueue.poll();
-                    rqueue.remove(min);
+                    rqueue.remove(fqueue.poll());
                 } else {
-                    int max = rqueue.poll();
-                    fqueue.remove(max);
+                    fqueue.remove(rqueue.poll());
                 }
-                qSize--;
             }
         }
 
         int[] answer = { 0, 0 };
 
-        if (qSize > 0) {
+        if (fqueue.size() > 0) {
             answer[0] = rqueue.peek();
             answer[1] = fqueue.peek();
         }
