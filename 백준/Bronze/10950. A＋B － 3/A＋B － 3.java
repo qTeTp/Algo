f751c5a61d874e1cc5299b45a8433cae29830f09
qTeTp <68058317+qTeTp@users.noreply.github.com>
2025-08-11 15:10:48 +0900

    [Bronze V] Title: A+B - 3, Time: 184 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        for (int i = 0; i < a; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            System.out.println(x + y);
+        }
+    }
+}
