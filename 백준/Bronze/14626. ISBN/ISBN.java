5cfae16b47fd4f6f58c757b8574115e1ac7738a1
qTeTp <68058317+qTeTp@users.noreply.github.com>
2025-08-12 10:06:23 +0900

    [Bronze I] Title: ISBN, Time: 168 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Bronze/14626. ISBN/ISBN.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // ISBN
+        // 13자리 숫자가 있는데 마지막은 체크기호
+        // 이 마지막 숫자는 앞의 각 자리수에 1, 3, 1, 3,...을 곱한 것을 모두 더하고
+        // 그 값을 10으로 나눈 나머지가 0이 되도록 만드는 숫자 m을 사용
+        // 중간에 훼손된 숫자가 있을 시 그 숫자를 찾아내 리턴
+
+        String str = sc.nextLine();
+
+        int sum = 0;
+        int starLocation = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == '*') {
+                starLocation = i;
+                continue;
+            }
+
+            int tmp = ch - '0';
+            if (i % 2 == 0) {
+                sum += tmp;
+            } else {
+                sum += tmp * 3;
+            }
+        }
+
+        int ye = (starLocation % 2 == 0) ? 1 : 3;
+
+        for (int i = 0; i <= 9; i++) {
+            if ((sum + i * ye) % 10 == 0) {
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+}
