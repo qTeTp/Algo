7f2234406aa46f6ed55e26daf399465ce68b618d
qTeTp <68058317+qTeTp@users.noreply.github.com>
2025-08-27 13:34:26 +0900

    [Bronze V] Title: 아스키 코드, Time: 168 ms, Memory: 17620 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    // 백준 11654 아스키코드
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char ch = sc.next().charAt(0);
+        System.out.println(Integer.valueOf(ch));
+    }
+}
