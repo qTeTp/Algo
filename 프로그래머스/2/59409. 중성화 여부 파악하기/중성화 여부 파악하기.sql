cfc86433eccf908c5c02a654e5ffcca0538f9986
qTeTp <68058317+qTeTp@users.noreply.github.com>
2026-04-19 16:52:21 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -0,0 +1,11 @@
+-- 중성화 여부, 아이디 순 정렬
+SELECT ANIMAL_ID,
+    NAME,
+    CASE 
+        WHEN SEX_UPON_INTAKE LIKE '%Neutered%'
+        OR SEX_UPON_INTAKE LIKE '%Spayed%'
+        THEN 'O'
+        ELSE 'X'
+END AS '중성화'
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID
